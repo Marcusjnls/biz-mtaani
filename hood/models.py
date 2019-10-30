2a2485e5ee70f0d8638bd0c935589201285cdcf7
Marcus Jean-Louis <marcusjnls@gmail.com>
2019-10-30 15:28:34 +0300

    Create Emergency contacts model

--- hood/models.py
+++ hood/models.py
@@ -71,3 +71,14 @@ class Business(models.Model):
     def __str__(self):
         return f'{self.name}'
 
+
+class EmergencyContacts(models.Model):
+    name = models.CharField(max_length=30)
+    contacts = models.CharField(max_length=20)
+    email = models.EmailField()
+    neighborhood_contact = models.ForeignKey('Neighborhood',on_delete=models.CASCADE)
+
+    def __str__(self):
+        return f'{self.name},{self.email}'
+
+
